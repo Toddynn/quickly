fed65872558210f3c2825afc3fbbd962e9b818b1
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2025-03-25 14:10:09 -0300

    feat :sparkles: add navbar component

---
+++ src/app/(private)/components/navbar/index.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import AppLogo from '@/components/logo';
+import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle, useDisclosure } from '@heroui/react';
+import { LucideDoorOpen } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function AdminNavbar() {
+	const pathName = usePathname();
+	const { isOpen, onOpenChange } = useDisclosure();
+	const menuItems = [
+		{ label: 'Início', href: '/dashboard' },
+		{ label: 'Agendamentos', href: '/schedules' },
+		{ label: 'Funcionários', href: '/employees' },
+		{ label: 'Serviços', href: '/services' },
+	];
+
+	return (
+		<Navbar
+			isBordered
+			isMenuOpen={isOpen}
+			onMenuOpenChange={onOpenChange}
+			classNames={{
+				item: [
+					'flex',
+					'relative',
+					'h-full',
+					'items-center',
+					"data-[active=true]:after:content-['']",
+					'data-[active=true]:after:absolute',
+					'data-[active=true]:after:bottom-0',
+					'data-[active=true]:after:left-0',
+					'data-[active=true]:after:right-0',
+					'data-[active=true]:after:h-[2px]',
+					'data-[active=true]:after:rounded-[2px]',
+					'data-[active=true]:after:bg-primary',
+				],
+			}}
+		>
+			<NavbarContent className="sm:hidden" justify="start">
+				<NavbarMenuToggle aria-label={isOpen ? 'Close menu' : 'Open menu'} />
+				<NavbarBrand>
+					<AppLogo />
+					<p className="font-bold text-inherit">QUICKLY</p>
+				</NavbarBrand>
+			</NavbarContent>
+
+			<NavbarContent className="hidden sm:flex gap-4" justify="center">
+				<NavbarBrand>
+					<AppLogo />
+					<p className="font-bold text-inherit">QUICKLY</p>
+				</NavbarBrand>
+				{menuItems.map((menu_item, idx) => (
+					<NavbarItem key={'pc-menu-item' + idx} isActive={pathName === menu_item.href}>
+						<Link href={menu_item.href}>{menu_item.label}</Link>
+					</NavbarItem>
+				))}
+			</NavbarContent>
+
+			<NavbarContent className="hidden sm:flex" justify="end">
+				<NavbarItem>
+					<Button color="danger" variant="light" endContent={<LucideDoorOpen size={16} />}>
+						Sair
+					</Button>
+				</NavbarItem>
+			</NavbarContent>
+
+			<NavbarMenu>
+				{menuItems.map((menu_item, index) => {
+					const is_active = pathName === menu_item.href;
+					return (
+						<NavbarMenuItem key={'mobile-menu-item' + index}>
+							<Button
+								as={Link}
+								variant={is_active ? 'bordered' : 'light'}
+								color={is_active ? 'primary' : 'default'}
+								radius="none"
+								className="w-full text-inherit font-medium justify-start border-x-0 border-t-0"
+								href={menu_item.href}
+							>
+								{menu_item.label}
+							</Button>
+						</NavbarMenuItem>
+					);
+				})}
+				<NavbarMenuItem>
+					<Button color="danger" variant="flat" className="w-full justify-start" endContent={<LucideDoorOpen size={16} />}>
+						Sair
+					</Button>
+				</NavbarMenuItem>
+			</NavbarMenu>
+		</Navbar>
+	);
+}
