944c2862a0f1714d7ad1915afa33ccad5200a0fe
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2025-03-28 14:13:43 -0300

    feat :sparkles: import fonts from correct place

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
+import { dmSansFont, gugiFont } from '@/shared/constants/fonts';
 import { Metadata } from 'next';
-import { DM_Sans } from 'next/font/google';
 import { twMerge } from 'tailwind-merge';
 import './globals.css';
 import Providers from './providers';
@@ -9,19 +9,13 @@ export const metadata: Metadata = {
 	description: 'Gestão de agendamentos simples e fácil, como deve ser.',
 };
 
-const dmSansFont = DM_Sans({
-	subsets: ['latin'],
-	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900', '1000'],
-	variable: '--font-dm-sans',
-});
-
 export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
 	return (
-		<html suppressHydrationWarning lang="ptbr" className={twMerge(dmSansFont.variable, 'font-dm_sans')}>
+		<html suppressHydrationWarning lang="ptbr" className={twMerge(dmSansFont.variable, gugiFont.variable, 'font-dm_sans')}>
 			<body suppressHydrationWarning className={'scroll-smooth antialiased'}>
 				<Providers>{children}</Providers>
 			</body>
