99643969b0dff0e53f3c0195fc7d04bc620b9f57
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2025-03-28 15:51:11 -0300

    feat :sparkles: adjust aside login

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -2,9 +2,8 @@
 
 import AppLogo from '@/components/logo';
 import { ThemeSwitcher } from '@/components/ui/theme-switch';
-import { Button, Card, CardBody, CardHeader, Checkbox, Divider, Input } from '@heroui/react';
-import { LucideBanknote, LucideCalendar, LucideCalendarClock, LucideEye } from 'lucide-react';
-import Image from 'next/image';
+import { Button, Card, CardBody, CardFooter, CardHeader, Checkbox, Input } from '@heroui/react';
+import { LucideBarChart2, LucideEye } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Login() {
@@ -49,69 +48,35 @@ export default function Login() {
 			</aside>
 			<aside className="relative flex flex-col overflow-hidden gap-12 size-full p-20 text-foreground bg-[#2A2A2A] rounded-xl">
 				<div className="absolute -top-16 -right-16 rounded-full bg-slate-100/10 size-96 z-0" />
-				<div className="flex items-center gap-4 size-full">
-					<div className="size-full flex justify-center flex-col gap-6">
-						<Card shadow="sm" className="light translate-x-10 z-10">
-							<CardHeader className="gap-2 p-6 flex-wrap font-medium text-lg">
-								Agendamentos <LucideCalendar size={18} />
-								<Divider />
-							</CardHeader>
-							<CardBody className="gap-4">
-								<div className="flex items-center overflow-hidden w-full justify-between gap-4">
-									<Button isIconOnly className="shrink-0 dark" size="lg" radius="full">
-										<LucideCalendarClock size={18} />
-									</Button>
-									<div className="flex flex-col shrink-0 justify-start items-start">
-										<h1 className="font-medium text-base">Daniel da silva</h1>
-										<h3 className="text-sm text-default-600">22 de janeiro de {new Date().getFullYear()}</h3>
-									</div>
-									<Button className="shrink-0" radius="full">
-										Agendamento
-									</Button>
-								</div>
-								<div className="flex items-center overflow-hidden justify-between gap-4">
-									<Button isIconOnly className="shrink-0 dark" size="lg" radius="full">
-										<LucideCalendarClock size={18} />
-									</Button>
-									<div className="flex flex-col shrink-0 justify-start items-start">
-										<h1 className="font-medium text-base">Emanoel Castanha</h1>
-										<h3 className="text-sm text-default-600">27 de janeiro de {new Date().getFullYear()}</h3>
-									</div>
-									<Button className="shrink-0" radius="full">
-										Agendamento
-									</Button>
-								</div>
-							</CardBody>
-						</Card>
-						<Card shadow="sm" className="light translate-x-16 z-10">
-							<CardBody className="flex-row gap-4">
-								<Button isIconOnly className="shrink-0 dark" size="lg" radius="full">
-									<LucideBanknote size={26} />
-								</Button>
-								<div className="flex flex-col shrink-0 justify-start items-start">
-									<h1 className="font-medium text-base">Ganhos</h1>
-									<h3 className="text-xl font-bold text-success-600">R$ 350.40</h3>
-								</div>
-							</CardBody>
-						</Card>
-					</div>
-					<div className="relative rounded-2xl w-2/3 overflow-hidden h-96">
-						<Image
-							src="/assets/images/login.jpg"
-							alt="Men looking at phone"
-							className="object-cover object-center"
-							sizes="1920x1080"
-							quality={100}
-							fill
-							priority
-						/>
+
+				<div className="relative size-full flex justify-center flex-col gap-6">
+					<Card shadow="sm" className="light relative">
+						<CardHeader className="gap-2 p-6 flex-wrap text-4xl text-success-700 font-bold">Metas Rápidas de Agendamento</CardHeader>
+						<CardBody className="p-6">
+							<p className="text-ellipsis font-normal text-lg text-pretty">
+								Alcance eficiência no agendamento. Use o nosso sistema de agendamento para organizar compromissos de forma prática e
+								transparente.
+							</p>
+						</CardBody>
+						<CardFooter></CardFooter>
+					</Card>
+					<div className="light bg-slate-50 p-6 rounded-2xl w-72 absolute bottom-8 -right-6 flex-row flex gap-2 text-success-700 items-center ">
+						<div>
+							<Button radius="full" size="lg" isIconOnly>
+								<LucideBarChart2 size={24} />
+							</Button>
+						</div>
+						<div className="flex flex-col ">
+							<h3 className="text-sm font-semibold">Ganhos</h3>
+							<h1 className="text-2xl font-bold">R$ 350.40</h1>
+						</div>
 					</div>
 				</div>
+
 				<div className="flex flex-col dark:text-foreground text-background gap-12 items-center">
-					<h1 className="text-5xl text-pretty font-bold">Agendamentos simplificados e rápidos</h1>
+					<h1 className="text-3xl text-pretty font-bold">Agendamentos simplificados e rápidos</h1>
 					<p className="text-pretty text-base font-medium">
-						Garanta eficiência e organização com nosso sistema avançado de agendamento. Planeje e gerencie compromissos de forma prática e
-						ágil, economizando tempo e aumentando sua produtividade.
+						Analisar padrões de agendamentos anteriores garante que os negócios sempre tomem as decisões certas.
 					</p>
 					<AppLogo className="text-3xl" />
 				</div>
