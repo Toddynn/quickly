63b132ed4e4e10a7f12b38c442f4730a43307e33
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2025-03-28 22:59:59 -0300

    feat :sparkles: accept any image host

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
-const nextConfig = {
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
 	images: {
 		remotePatterns: [
 			/* {
 				hostname: process.env.NEXT_PUBLIC_BACKEND_DOMAIN,
 			}, */
 			{
-				hostname: process.env.NEXT_PUBLIC_APP_DOMAIN,
+				hostname: String(process.env.NEXT_PUBLIC_APP_DOMAIN),
+			},
+			{
+				protocol: 'https',
+				hostname: '**',
+				pathname: '**',
 			},
 		],
 	},
