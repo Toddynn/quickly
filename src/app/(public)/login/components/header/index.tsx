8f2436eb2383b1681e29d62d094ddea8c5a782f2
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2025-03-31 13:33:34 -0300

    feat :sparkles: add new login header component

---
+++ src/app/(public)/login/components/header/index.tsx
@@ -0,0 +1,19 @@
+'use client';
+import AppLogo from '@/components/logo';
+import { APP_ROUTES } from '@/shared/constants/app-routes';
+import { Button } from '@heroui/react';
+import { LucideArrowLeft } from 'lucide-react';
+import Link from 'next/link';
+
+export default function LoginHeader() {
+	return (
+		<div className="flex items-center gap-2">
+			<Link href={APP_ROUTES.PUBLIC.MURAL_CHOICE}>
+				<Button variant="light" isIconOnly as="div">
+					<LucideArrowLeft size={18} />
+				</Button>
+			</Link>
+			<AppLogo className="text-5xl" />
+		</div>
+	);
+}
--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,14 +1,12 @@
-import AppLogo from '@/components/logo';
-import { ThemeSwitcher } from '@/components/ui/theme-switch';
 import LoginAsideCover from './components/aside';
 import LoginForm from './components/form';
+import LoginHeader from './components/header';
 
 export default async function Login() {
 	return (
 		<main className="flex h-screen items-center lg:gap-12 p-12 lg:p-20">
 			<aside className="flex flex-col items-start gap-12 size-full md:p-12 lg:p-20">
-				<AppLogo className="text-5xl" />
-				<ThemeSwitcher />
+				<LoginHeader />
 				<LoginForm />
 			</aside>
 			<LoginAsideCover />
