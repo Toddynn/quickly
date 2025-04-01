b2ff5ea5e5c0b372b2db2f678b0692badc376464
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2025-04-01 14:11:55 -0300

    feat :sparkles: add skeleton of services page

--- src/app/(private)/services/page.tsx
+++ src/app/(private)/services/page.tsx
@@ -1,3 +1,21 @@
+'use client';
+
+import { Card, CardHeader } from '@heroui/react';
+import Image from 'next/image';
+
 export default function Services() {
-	return <div className="size-full flex gap-6 p-4">Services</div>;
+	return (
+		<div className="size-full flex gap-6 p-4">
+			<Card>
+				<CardHeader>
+					<Image
+						alt="image"
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgiaafSHQ3Z7C-jgJv0k9PyKzguLk9GiXsmQ&s"
+						width={200}
+						height={200}
+					/>
+				</CardHeader>
+			</Card>
+		</div>
+	);
 }
