729cbad0806f836cc43f892ca12a938927aab31c
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2025-04-02 13:45:40 -0300

    feat :sparkles: adjust services chart skeleton

--- src/app/(private)/dashboard/components/charts/services-chart/index.tsx
+++ src/app/(private)/dashboard/components/charts/services-chart/index.tsx
@@ -1,27 +1,29 @@
 'use client';
 
-import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
-import { Bar, BarChart, XAxis } from 'recharts';
+import { ChartConfig, ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
+import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
 
 const chartData = [
-	{ date: '2024-07-15', running: 450, swimming: 300 },
-	{ date: '2024-07-16', running: 380, swimming: 420 },
-	{ date: '2024-07-17', running: 520, swimming: 120 },
-	{ date: '2024-07-18', running: 140, swimming: 550 },
-	{ date: '2024-07-19', running: 600, swimming: 350 },
-	{ date: '2024-07-20', running: 480, swimming: 400 },
+	{ month: 'Janeiro', desktop: 186, mobile: 80 },
+	{ month: 'Fevereiro', desktop: 305, mobile: 200 },
+	{ month: 'Março', desktop: 237, mobile: 120 },
+	{ month: 'Abril', desktop: 73, mobile: 190 },
+	{ month: 'Maio', desktop: 209, mobile: 130 },
+	{ month: 'Junho', desktop: 214, mobile: 140 },
+	{ month: 'Julho', desktop: 214, mobile: 140 },
+	{ month: 'Agosto', desktop: 214, mobile: 140 },
+	{ month: 'Setembro', desktop: 214, mobile: 140 },
+	{ month: 'Outubro', desktop: 214, mobile: 140 },
+	{ month: 'Novembro', desktop: 214, mobile: 140 },
+	{ month: 'Dezembro', desktop: 214, mobile: 140 },
 ];
-
 const chartConfig = {
-	activities: {
-		label: 'Activities',
-	},
-	running: {
-		label: 'Running',
+	desktop: {
+		label: 'Desktop',
 		color: 'hsl(var(--chart-1))',
 	},
-	swimming: {
-		label: 'Swimming',
+	mobile: {
+		label: 'Mobile',
 		color: 'hsl(var(--chart-2))',
 	},
 } satisfies ChartConfig;
@@ -29,22 +31,21 @@ const chartConfig = {
 export default function ServicesChart() {
 	return (
 		<ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-			<BarChart accessibilityLayer data={chartData}>
-				<XAxis
-					dataKey="date"
-					tickLine={false}
-					tickMargin={10}
-					axisLine={false}
-					tickFormatter={(value) => {
-						return new Date(value).toLocaleDateString('en-US', {
-							weekday: 'short',
-						});
-					}}
-				/>
-				<Bar dataKey="running" stackId="a" fill="var(--color-running)" radius={[0, 0, 4, 4]} />
-				<Bar dataKey="swimming" stackId="a" fill="var(--color-swimming)" radius={[4, 4, 0, 0]} />
-				<ChartTooltip content={<ChartTooltipContent labelKey="activities" indicator="line" />} cursor={false} defaultIndex={1} />
-			</BarChart>
+			<AreaChart
+				accessibilityLayer
+				data={chartData}
+				margin={{
+					left: 12,
+					right: 12,
+				}}
+			>
+				<CartesianGrid vertical={false} />
+				<XAxis dataKey="month" tickLine={false} axisLine={false} tickMargin={8} tickFormatter={(value) => value.slice(0, 3)} />
+				<ChartTooltip cursor={false} content={<ChartTooltipContent indicator="dot" />} />
+				<ChartLegend content={<ChartLegendContent />} />
+				<Area dataKey="mobile" type="bump" fill="var(--color-mobile)" fillOpacity={0.6} stroke="var(--color-mobile)" stackId="a" />
+				<Area dataKey="desktop" type="bump" fill="var(--color-desktop)" fillOpacity={0.6} stroke="var(--color-desktop)" stackId="a" />
+			</AreaChart>
 		</ChartContainer>
 	);
 }
