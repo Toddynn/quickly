d9287b6cc6e0f5ba36112721aae2abbe270f9b7c
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2025-04-02 22:21:41 -0300

    feat :sparkles: add new least chosen service card component

---
+++ src/app/(private)/dashboard/components/cards/least-chosen-service-card/index.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { Button, Card, CardBody, CardFooter, CardHeader } from '@heroui/react';
+import { LucideArrowDownRight, LucideChevronsUpDown, LucideThumbsDown } from 'lucide-react';
+
+export default function LeastChosenServiceCard() {
+	return (
+		<Card shadow="sm" className="w-full h-full">
+			<CardHeader className="flex-col items-start">
+				<div className="flex items-center justify-between gap-2 w-full">
+					<h1 className="text-xl font-medium">
+						Serviço <span className="text-danger">menos</span> escolhido
+					</h1>
+					<Button isIconOnly radius="full" size="lg" variant="flat" color="danger" className="shrink-0">
+						<LucideThumbsDown size={20} />
+					</Button>
+				</div>
+				<div className="flex items-center gap-2">
+					<Button endContent={<LucideChevronsUpDown size={14} />} size="sm" radius="sm" variant="bordered" className="border-1 text-default-600">
+						<h3 className="text-base text-default-600 capitalize">Nessa semana</h3>
+					</Button>
+				</div>
+			</CardHeader>
+			<CardBody>
+				<h1 className="text-3xl font-bold">Barba</h1>
+			</CardBody>
+			<CardFooter className="pt-0">
+				<p className="flex whitespace-normal flex-wrap items-center gap-1 text-lg font-normal text-default-600 text-pretty">
+					<span className="text-danger font-medium flex items-center gap-1">
+						<LucideArrowDownRight size={18} /> 2%
+					</span>
+					em comparação com o outro serviço
+				</p>
+			</CardFooter>
+		</Card>
+	);
+}
